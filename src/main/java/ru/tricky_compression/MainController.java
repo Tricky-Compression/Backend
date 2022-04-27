eaef13732929ffd1ee38f41d901b2515dcc48149
Nikita Denisov <nikita160412@yandex.ru>
2022-04-27 17:35:05 +0300

    Try streaming data with producer/consumer pattern

--- src/main/java/ru/tricky_compression/MainController.java
+++ src/main/java/ru/tricky_compression/MainController.java
@@ -1,12 +1,15 @@
 package ru.tricky_compression;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -63,5 +66,43 @@ public class MainController {
 	@GetMapping("api/download")
 	public Element download(@RequestParam(value = "id") int id) {
 		return dataBase.get(id);
+	}
+
+	@GetMapping (value = "api/data", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<StreamingResponseBody> data(final HttpServletResponse response) {
+		response.setContentType("application/json");
+		StreamingResponseBody stream = out -> {
+			try {
+				for (int i = 0; i < 10; i++) {
+					String content = "{\"counter\": " + i + "}\n";
+					out.write(content.getBytes());
+					out.flush();
+				}
+				out.close();
+			} catch (final Exception e) {
+				e.printStackTrace();
+			}
+		};
+		return new ResponseEntity(stream, HttpStatus.OK);
+	}
+
+	@GetMapping(value = "api/consume", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> consume() throws IOException {
+		URL url = new URL("http://localhost:8080/api/data");
+		URLConnection connection = url.openConnection();
+		connection.setDoOutput(true);
+
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(connection.getInputStream())
+		);
+
+		String decodedString;
+		while ((decodedString = in.readLine()) != null) {
+			System.out.println(decodedString);
+		}
+
+		in.close();
+
+		return new ResponseEntity("ok", HttpStatus.OK);
 	}
 }
