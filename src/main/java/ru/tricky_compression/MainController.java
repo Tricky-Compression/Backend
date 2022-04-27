08dd347aa1815c12c21062944700262afa23d3b3
Nikita Denisov <nikita160412@yandex.ru>
2022-04-27 16:52:02 +0300

    Add possibility to download single file

--- src/main/java/ru/tricky_compression/MainController.java
+++ src/main/java/ru/tricky_compression/MainController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.*;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -27,19 +30,34 @@ public class MainController {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
-	@PostMapping(value ="api/upload")
+	@PostMapping("api/upload")
 	public ResponseEntity<String> upload(@RequestBody DataFrame data) {
 		dataBase.add(new Element(data.getContent()));
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 
-	@PostMapping(value = "api/upload/single_file")
+	@PostMapping("api/upload/single_file")
 	public ResponseEntity<String> uploadSingleFile(@RequestBody File file) throws IOException {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(file.getFilename()));
 		writer.write(file.getData());
 
 		writer.close();
 		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	public String getFileContent(String filename) throws IOException {
+		Path path = Paths.get(filename);
+		return Files.readAllLines(path).get(0);
+	}
+
+	@GetMapping("api/download/single_file")
+	public ResponseEntity<String> downloadSingleFile(@RequestParam(value = "filename") String filename) {
+		try {
+			String data = getFileContent(filename);
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body(data);
+		} catch (IOException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
 	}
 
 	@GetMapping("api/download")
