d3d933512806c27334da0d2de7b16f487841c938
Nikita Denisov <nikita160412@yandex.ru>
2022-05-04 21:48:25 +0300

    Fix fileController

--- src/main/java/ru/tricky_compression/FileManagerController.java
+++ src/main/java/ru/tricky_compression/FileManagerController.java
@@ -6,34 +6,32 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api")
 public class FileManagerController {
-	@PostMapping("/upload/single_file")
-	public ResponseEntity<String> uploadSingleFile(@RequestBody File file) throws IOException {
-		BufferedWriter writer = new BufferedWriter(new FileWriter(file.getFilename()));
-		writer.write(file.getData());
+    private static final String prefix = "/app";
 
-		writer.close();
-		return ResponseEntity.status(HttpStatus.CREATED).build();
-	}
+    @PostMapping("/upload/single_file")
+    public ResponseEntity<String> uploadSingleFile(@RequestBody File file) {
+        try (FileWriter fileWriter = new FileWriter(prefix + file.getFilename());
+             BufferedWriter writer = new BufferedWriter(fileWriter)) {
+            writer.write(file.getData());
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
 
-	public String getFileContent(String filename) throws IOException {
-		Path path = Paths.get(filename);
-		return Files.readAllLines(path).get(0);
-	}
-
-	@GetMapping("/download/single_file")
-	public ResponseEntity<String> downloadSingleFile(@RequestParam(value = "filename") String filename) {
-		try {
-			String data = getFileContent(filename);
-			return ResponseEntity.status(HttpStatus.ACCEPTED).body(data);
-		} catch (IOException e) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-		}
-	}
+    @GetMapping("/download/single_file")
+    public ResponseEntity<String> downloadSingleFile(@RequestParam(value = "filename") String filename) {
+        try {
+            String data = String.join("\n", Files.readAllLines(Paths.get(filename)));
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(data);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
 
 }
