84c2fa67037baeb7bbef4d35e490cc77407a98d1
APozdniakov <pas.9250306384@gmail.com>
2022-05-08 11:05:55 +0300

    Fix file downloading

--- src/main/java/ru/tricky_compression/FileManagerController.java
+++ src/main/java/ru/tricky_compression/FileManagerController.java
@@ -9,16 +9,21 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api")
 public class FileManagerController {
     private static final String prefix = "/file_storage/";
 
+    private Path getPath(String filename) {
+        return Path.of(prefix, filename);
+    }
+
     @PostMapping("/upload/single_file")
     public ResponseEntity<String> uploadSingleFile(@RequestBody File file) {
         try {
-            Path path = Path.of(prefix, file.getFilename());
+            Path path = getPath(file.getFilename());
             Files.createDirectories(path.getParent());
             Files.write(
                     path,
@@ -28,17 +33,18 @@ public class FileManagerController {
             );
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 
     @GetMapping("/download/single_file")
     public ResponseEntity<String> downloadSingleFile(@RequestParam(value = "filename") String filename) {
         try {
-            String data = String.join("\n", Files.readAllLines(Paths.get(filename)));
-            return ResponseEntity.status(HttpStatus.ACCEPTED).body(data);
+            Path path = getPath(filename);
+            byte[] data = Files.readAllBytes(Paths.get(path.toString()));
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(Arrays.toString(data));
         } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 
