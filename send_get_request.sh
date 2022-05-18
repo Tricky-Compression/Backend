a45020a1b47095f440873eac6c78d4add0ff2052
Nikita Denisov <nikita160412@yandex.ru>
2022-05-18 18:05:54 +0300

    Remove scripts

--- send_get_request.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl http://51.250.23.237:1337/api/download/single_file?filename=test-large-file
--- send_post_request.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl -X POST -H "Content-Type: application/json" --data-binary "@large_post_request.json" http://51.250.23.237:1337/api/upload/single_file
